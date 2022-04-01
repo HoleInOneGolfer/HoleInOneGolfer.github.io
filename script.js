f8404a9da22b9d2b9c94d4c3904e39767380f6e8
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-04-01 13:38:36 -0500

    moved to organization

--- script.js
+++
@@ -1,9 +0,0 @@
-function gVC(event) {
-    //event.preventDefault();
-    data = new FormData(event.target);
-
-    console.log(data.get('color'))
-
-    event.target.action = "/Virtual-Card/" + `?to=${data.get("to")}&from=${data.get("from")}&occasion=${data.get("occasion")}`;
-    
-}
